6f12b569d838c7cdea265bced9d83805d0082cf9
erpranto <erpranto55@gmail.com>
2025-09-17 01:27:48 +0600

    Doubly linked list revise done

---
+++ Revision/Doubly_linked_list/doubly_linked_list.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    head->prev = newnode;
+    newnode->next = head;
+    head = head->prev;
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = tail->next;
+}
+
+void insert_at_any_position(Node *&head, int idx, int val)
+{
+    Node *newnode = new Node(val);
+    Node *tmp = head;
+    for (int i = 0; i < idx - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newnode->next = tmp->next;
+    tmp->next->prev = newnode;
+    tmp->next = newnode;
+    newnode->prev = tmp;
+}
+
+void delete_at_head(Node *&head, Node *&tail)
+{
+    Node *deletenode = head;
+    head = head->next;
+    delete deletenode;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+void delete_at_tail(Node *&head, Node *&tail)
+{
+    Node *deletenode = tail;
+    tail = tail->prev;
+    delete deletenode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+
+void delete_at_any_position(Node *&head, Node *&tail, int idx)
+{
+    Node *tmp = head;
+    for (int i = 0; i < idx - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deletenode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
+    delete deletenode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (cin >> val)
+    {
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    print_forward(head);
+    return 0;
+}
